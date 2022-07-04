034f4f20218b18f6aa0250f06f061b64f3f820ff
Mike Varga <mikevarga07@gmail.com>
2022-07-03 20:24:39 -0500

    Displayed user data from firebase on profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import { auth, signOut } from "../firebase";
+import { auth, signOut, firestore } from "../firebase";
 import { authStyle } from "../styles";
+import { doc, getDoc } from "firebase/firestore";
 
 const Profile = () => {
+  let [userData, setUserData] = useState('');
+
   const handleSignOut = () => {
     signOut(auth)
   }
 
+  useEffect(() => {
+    const getUser = async () => {
+      const docRef = doc(firestore, 'users', auth.currentUser.uid);
+      const docSnap = await getDoc(docRef);
+
+      if(docSnap.exists()) {
+        setUserData(docSnap.data());
+      }
+    }
+    getUser();
+  }, [])
+
+  console.log("USER Data", userData);
   return (
     <View>
-      <Text>Profile page</Text>
+      <Text>Name: {userData.name}</Text>
+      <Text>Score: {userData.score}</Text>
+      <Text>Email: {userData.email}</Text>
+      <Text>Username: {userData.username}</Text>
       <TouchableOpacity style={authStyle.submitButton} title="Signout" onPress={() => handleSignOut()}>
         <Text>Sign Out</Text>
       </TouchableOpacity>
